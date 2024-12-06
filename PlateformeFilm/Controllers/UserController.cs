74398412c95defd022d2dc9aa8a0d217fe86b789
Rzeigui Ahmed <ahmedrzeigui52@gmail.com>
2024-12-06 15:43:05 +0100

    Ajout des requettes Put et Delete dans UserController

--- PlateformeFilm/Controllers/UserController.cs
+++ PlateformeFilm/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using PlateformeFilm.Models;
 using PlateformeFilm.data;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -30,6 +31,7 @@ namespace PlateformeFilm.Controllers
         // on retourne la confiture
         return Ok(user);
     }
+
     //Ajouter des données 
     public class UserCreation
     {
@@ -55,5 +57,61 @@ namespace PlateformeFilm.Controllers
         // on retourne un code 201 pour indiquer que la création a bien eu lieu
         return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
     }
+
+
+    //Modifier un utilisateur déja existant 
+    [HttpPut("{id}")]
+    
+    public async Task<ActionResult<User>> PutUser(User userUpdate) // c pas <IActionResult<User>>
+    {
+        // on récupère la confiture que l'on souhaite modifier
+        User user = await _context.Users.FindAsync(userUpdate.Id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        // on met a jour les informations de la confiture
+
+        user.Id=userUpdate.Id;
+        user.Pseudo=userUpdate.Pseudo;
+        user.Password=userUpdate.Password;
+
+
+
+        // on indique a notre contexte que l'objet a été modifié
+        _context.Entry(user).State = EntityState.Modified;
+
+        try
+        {
+            // on enregistre les modifications
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            // si une erreur de concurrence survient on retourne un code 500
+            return StatusCode(500, "Erreur de concurrence");
+        }
+        // on retourne un code 200 pour indiquer que la modification a bien eu lieu
+        return Ok(user);
+    }
+    //Supprimer les Données 
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteConfiture(int id)
+    {
+        // on récupère la user que l'on souhaite supprimer
+        User user = await _context.Users.FindAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        // on indique a notre contexte que l'objet a été supprimé
+        _context.Users.Remove(user);
+        // on enregistre les modifications
+        await _context.SaveChangesAsync();
+        // on retourne un code 204 pour indiquer que la suppression a bien eu lieu
+        return NoContent();
+    }
+
     }
 }
